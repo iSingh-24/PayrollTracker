3e436d65d39b5df7a0b9c673e982474e7251d86e
Inderprit Singh <singhinderprit1992@gmail.com>
2024-09-24 19:01:56 -0500

    basic setup and some cleanup for boilerplate for this project

---
+++ bin/seed.js
@@ -0,0 +1,22 @@
+const db = require("../server/db/db");
+
+const init = async () => {
+  try {
+    await db.sync({ force: true }); //this will drop and recreate tables
+    console.log("Database was synced successfully");
+    // const testUsers = [
+    //   { firstName: "Madara", lastName: "Uchiha", email: "uchiha@gmail.com" },
+    //   { firstName: "Ip", lastName: "Bryant", email: "Mamba@gmail.com" },
+    //   { firstName: "Thor", lastName: "Vinland", email: "Valhalla@gmail.com" },
+    // ];
+
+    const testUserPromises = testUsers.map((user) => User.create(user)); //this will map over and create a user object for each user
+    const createdTestUsers = await Promise.all(testUserPromises); //this will wait for the creation of the users to be complete
+
+    console.log("Users and Workouts were created successfully ");
+  } catch (err) {
+    console.log(`Error syncing the database: ${err}`);
+  }
+};
+
+init();
