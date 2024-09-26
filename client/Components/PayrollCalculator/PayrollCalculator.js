e6c9a9b74b3d39235d37d0a09846510c3ab48194
Inderprit Singh <singhinderprit1992@gmail.com>
2024-09-25 21:02:43 -0500

    visual for payroll calc

--- client/Components/PayrollCalculator/PayrollCalculator.js
+++ client/Components/PayrollCalculator/PayrollCalculator.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./payrollCalc.css";
 
 const PayrollCalculator = () => {
-  return <div>Payroll Calc Component</div>;
+  return <div className="container">Payroll Calc Component</div>;
 };
 
 export default PayrollCalculator;
