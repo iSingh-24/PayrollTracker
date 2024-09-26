1cd8e8cb918c6e66be1830795209ff2b2d49ca1f
Inderprit Singh <singhinderprit1992@gmail.com>
2024-09-25 22:06:43 -0500

    calculator setup

--- client/Components/PayrollCalculator/PayrollCalculator.js
+++ client/Components/PayrollCalculator/PayrollCalculator.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./payrollCalc.css";
 
+/**
+ * TODO
+ *
+ * 1) Instead of input tags used to track the time, figure out how to use flatpickr library or timepicker.js
+ */
+
 const PayrollCalculator = () => {
-  return <div className="container">Payroll Calc Component</div>;
+  return (
+    <div className="container">
+      <label>Enter Start Time:</label>
+      <br></br>
+      <label>Enter End Time:</label>
+      <input type="time" />
+      <br></br>
+    </div>
+  );
 };
 
 export default PayrollCalculator;
