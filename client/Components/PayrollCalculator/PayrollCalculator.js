5d9a00d7f5c606e580963c59f56b9785f278f304
Inderprit Singh <singhinderprit1992@gmail.com>
2024-09-25 22:33:36 -0500

    added a function that will track the difference of the start time and end time and let you know how many hours the employee worked

--- client/Components/PayrollCalculator/PayrollCalculator.js
+++ client/Components/PayrollCalculator/PayrollCalculator.js
@@ -5,15 +5,33 @@ import "./payrollCalc.css";
  * TODO
  *
  * 1) Instead of input tags used to track the time, figure out how to use flatpickr library or timepicker.js
+ * 2) Make a function that will find the decimal value of the minutes and hours
  */
 
 const PayrollCalculator = () => {
   const [startTime, setStartTime] = useState("");
   const [endTime, setEndTime] = useState("");
 
+  const TotalHoursCalc = (startTime, endTime) => {
+    const start = new Date(`1940-03-01T${startTime}`);
+    const end = new Date(`1940-03-01T${endTime}`);
+
+    const totalHoursMilli = end - start;
+
+    const totalHours = totalHoursMilli / (1000 * 60 * 60);
+
+    const hours = Math.floor(totalHours); //this will give you the hours as a whole
+    const minutes = Math.round((totalHours - hours) * 60);
+
+    return `Employee worked ${hours} hours and ${minutes} minutes`;
+  };
+
+  const calculateHoursHandler = () => {
+    console.log(TotalHoursCalc(startTime, endTime));
+  };
+
   const onStartChangeHandler = ({ target }) => {
     console.log(target.value, "this is the start time");
-    console.log(target, "here is target");
     const { value } = target;
     setStartTime(value);
   };
@@ -42,6 +60,9 @@ const PayrollCalculator = () => {
         lang="en-US"
       />
       <br></br>
+      <button type="button" onClick={() => calculateHoursHandler()}>
+        Calculate Hours
+      </button>
     </div>
   );
 };
