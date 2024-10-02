63a0c509e00f95b7d02ae863c20b9df5f7c4eecf
Inderprit Singh <singhinderprit1992@gmail.com>
2024-10-01 23:56:41 -0500

    set up useEffect and useState hooks

--- client/Components/Employee/SingleEmployee.js
+++ client/Components/Employee/SingleEmployee.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+//TODO: write useEffect hook to fetch a single employee from the database
+//TODO: Update this component so I don't have the employee passed down, instead I am going to find out a way to grab the employee from the URL
+//TODO: Check to see if I need to unmount the component as well
+
 const SingleEmployee = ({ employee, deleteEmployee }) => {
+  const [currentEmployee, setCurrentEmployee] = useState("");
+
+  useEffect(() => {
+    console.log("single employee page was mounted");
+  }, []);
+
   return employee ? (
     <div>
       {employee.firstName}
