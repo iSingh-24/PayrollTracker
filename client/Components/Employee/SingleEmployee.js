399f3cb18c48588f0d368ba2263df727fa921aee
Inderprit Singh <singhinderprit1992@gmail.com>
2024-10-01 23:49:38 -0500

    set up a path to single employee page for update

--- client/Components/Employee/SingleEmployee.js
+++ client/Components/Employee/SingleEmployee.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SingleEmployee = ({ employee, deleteEmployee }) => {
   return (
     <div>
       {employee.firstName}
       <button onClick={() => deleteEmployee(employee.id)}>X</button>
+      <Link to={`/employees/${employee.id}`}>
+        <button type="button">Update Employee</button>
+      </Link>
     </div>
   );
 };
