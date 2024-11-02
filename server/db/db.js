d41175d00fa086cbce2a67af2a3a32b6ca99abce
Inderprit Singh <singhinderprit1992@gmail.com>
2024-11-02 00:55:16 -0500

    corrections

--- server/db/db.js
+++ server/db/db.js
@@ -3,26 +3,9 @@ const config = {
   logging: false, // Disables SQL query logging in the console
 };
 
-// const db = new Sequelize(
-//   process.env.DATABASE_URL || "postgres://localhost/payrolltracker",
-//   config
-// );
-
-const isProduction = process.env.NODE_ENV === "production";
-
 const db = new Sequelize(
   process.env.DATABASE_URL || "postgres://localhost/payrolltracker",
-  {
-    ...config,
-    dialectOptions: {
-      ssl: isProduction
-        ? {
-            require: true,
-            rejectUnauthorized: false, // Adjust based on your security needs
-          }
-        : false, // Disable SSL for local development
-    },
-  }
+  config
 );
 
 module.exports = db;
