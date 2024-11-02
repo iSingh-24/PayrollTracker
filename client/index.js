832f1e929e7f6aed66794e1224ce1f22b727664b
Inderprit Singh <singhinderprit1992@gmail.com>
2024-11-02 00:09:38 -0500

    note

--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./Components/App";
 import { HashRouter } from "react-router-dom";
 
+//note
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
